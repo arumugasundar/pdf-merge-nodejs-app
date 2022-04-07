16dcf4fbe82bce9ed545e16b14dffeba520e0d6a
arumugasundar <arumugasundar29@gmail.com>
2022-04-07 22:20:03 +0530

    API Configured v2.0

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
+var bodyParser = require('body-parser')
 const app = express();
 const MERGE_PDFS_API = require('./routes/merge-pdfs-api');
 
-app.use(express.json({limit: '20mb'}));
-app.use(express.urlencoded({ extended: false, limit: '20mb' }));
+// app.use(express.json({limit: '20mb'}));
+// app.use(express.urlencoded({ extended: false, limit: '20mb' }));
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
 
 app.use((req,res,next) => {
     res.setHeader("Access-Control-Allow-Origin","*");
--- routes/merge-pdfs-api.js
+++ routes/merge-pdfs-api.js
@@ -2,45 +2,61 @@ const express = require("express");
 const https = require('https');
 const fs = require('fs');
 const PDFMerger = require('pdf-merger-js');
+const crypto = require('crypto');
 
 const router = express.Router();
 
-router.post('/', async (req,res) => {
-
-    const labels = req.body.labelUrl;
-    let merger = new PDFMerger();
-  
-    for( let i in labels){
-      const file = fs.createWriteStream('label.pdf');
-
-      try{
-        https.get(labels[i], async function(response) {response.pipe(file);});
-      }catch(error) {
-        console.log('Error in fetching pdfs from downloadable link', error);
-        res.status(500).send({error: error, errorText:"Error in fetching pdfs from downloadable link"});
-      };
-
-      try{
-        merger.add('label.pdf');
-      }catch(error) {
-        console.log('Error in merging pdfs', error);
-        res.status(500).send({error: error, errorText:"Error in merging pdfs"});
-      };
-
+router.post('/', (req,res) => {
+  const labels = req.body.labelUrl; /* array of pdf downloadable links */
+  try{
+    let merger = new PDFMerger(); /* variable used to merge and save pdfs */
+    let count = 0; /* variable to keep track of number of responses collected from downloadable links */
+    for(let i = 0;i<labels.length;i++){
+        const fileName = crypto.randomUUID() + '.pdf'; /* randomized file name */
+        const file = fs.createWriteStream(fileName,  {
+          flags: "w",
+          encoding: "utf8",
+          mode: 0o666,
+          autoClose: true,
+          emitClose: true,
+          start: 0
+        });
+        https.get(labels[i], (resp) => {
+          resp.pipe(file); /* store the response from get request to the file */
+          file.on('finish', () => { /* listening for file completion state */
+            try{
+              merger.add(fileName);  /* add the current file data into merger variable */
+              fs.unlinkSync(fileName); /* delete the file after storing its content to merger */
+              count += 1; /* incrementing number of collected response */
+              if( count == labels.length){ /* check whether number of response collected is equal to number of labels */
+                const mergedFileName = crypto.randomUUID() + '.pdf';
+                merger.save(mergedFileName).then(() => { /* send response after saving the merged file */
+                  try{
+                    let stat = fs.statSync(mergedFileName);
+                    res.setHeader('Content-Length', stat.size);
+                    res.setHeader('Content-Type', 'application/pdf');
+                    res.setHeader('Content-Disposition', 'attachment; filename=label.pdf');
+                    res.download(mergedFileName, function(err) { /* callback to delete the file after sending response */
+                      if (err) {
+                        console.log(err);
+                      }
+                      console.log('file deleted');
+                      fs.unlinkSync(mergedFileName);
+                    });
+                  }catch(err){
+                    console.log("error in saving merged pdf");
+                  }
+                });
+              }
+            }catch(err){
+              console.log("error in merging pdf");
+            }
+          });
+        });
     }
-
-    await merger.save('mergedLabels.pdf').catch((error) => {
-        console.log('Error in saving merged pdf', error);
-        res.status(500).send({error: error, errorText:"Error in saving merged pdf"});
-    });
+  }catch(err){
+    res.status(500).send({error:err.toString(),errorText:"Merged pdf not generated"});
+  }
+});
   
-    let file = fs.createReadStream('mergedLabels.pdf');
-    let stat = fs.statSync('mergedLabels.pdf');
-    res.setHeader('Content-Length', stat.size);
-    res.setHeader('Content-Type', 'application/pdf');
-    res.setHeader('Content-Disposition', 'attachment; filename=label.pdf');
-    file.pipe(res);
-  
-  });
-  
-  module.exports = router;
+module.exports = router;
